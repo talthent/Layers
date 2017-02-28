eab14f6b831b8a6fed49fbcc981909b63786663b
Tal Cohen <talthent@gmail.com>
2017-02-28 13:18:35 +0200

    fabric

--- Layers/AppDelegate.swift
+++ Layers/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
+        Fabric.with([Crashlytics.self])
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = ViewController(nibName: nil, bundle: nil)
         self.window?.backgroundColor = UIColor.white
